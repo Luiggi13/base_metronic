77be79ca061265bf5aa79529e5e5d907bb28cc71
Christian Llansola <christian.llansola@gmail.com>
2020-09-08 21:48:30 +0200

    init commit laravel metronic

---
+++ resources/views/pages/login.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" {{ Metronic::printAttrs('html') }} {{ Metronic::printClasses('html') }}>
+    <head>
+        <meta charset="utf-8"/>
+
+        {{-- Title Section --}}
+        <title>{{ config('app.name') }} | @yield('title', $page_title ?? '')</title>
+
+        {{-- Meta Data --}}
+        <meta name="description" content="@yield('page_description', $page_description ?? '')"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+        {{-- Favicon --}}
+        <link rel="shortcut icon" href="{{ asset('media/logos/favicon.ico') }}" />
+
+        {{-- Fonts --}}
+        {{ Metronic::getGoogleFontsInclude() }}
+
+        {{-- Global Theme Styles (used by all pages) --}}
+        @foreach(config('layout.resources.css') as $style)
+            <link href="{{ config('layout.self.rtl') ? asset(Metronic::rtlCssPath($style)) : asset($style) }}" rel="stylesheet" type="text/css"/>
+        @endforeach
+
+        {{-- Layout Themes (used by all pages) --}}
+        @foreach (Metronic::initThemes() as $theme)
+            <link href="{{ config('layout.self.rtl') ? asset(Metronic::rtlCssPath($theme)) : asset($theme) }}" rel="stylesheet" type="text/css"/>
+        @endforeach
+
+        {{-- Includable CSS --}}
+  <link href="{{ asset('css/pages/login/login-3.css') }}" rel="stylesheet">
+
+        @yield('styles')
+    </head>
+	<!--end::Head-->
+	<!--begin::Body-->
+	<body {{ Metronic::printAttrs('body') }} {{ Metronic::printClasses('body') }}>
+		<!--begin::Main-->
+		<div class="d-flex flex-column flex-root">
+			<!--begin::Login-->
+			<div class="login login-3 wizard d-flex flex-column flex-lg-row flex-column-fluid">
+				<!--begin::Aside-->
+				<div class="login-aside d-flex flex-column flex-row-auto">
+					<!--begin::Aside Top-->
+					<div class="d-flex flex-column-auto flex-column pt-lg-40 pt-15">
+						<!--begin::Aside header-->
+						<a href="#" class="login-logo text-center pt-lg-25 pb-10">
+							<img src="{{ asset('/media/logos/logo-1.png') }}" class="max-h-70px" alt="" />
+						</a>
+						<!--end::Aside header-->
+						<!--begin::Aside Title-->
+						<h3 class="font-weight-bolder text-center font-size-h4 text-dark-50 line-height-xl">User Experience &amp; Interface Design
+						<br />Strategy SaaS Solutions</h3>
+						<!--end::Aside Title-->
+					</div>
+					<!--end::Aside Top-->
+					<!--begin::Aside Bottom-->
+					{{--  <div class="aside-img d-flex flex-row-fluid bgi-no-repeat bgi-position-x-center" style="background-position-y: calc(100% + 5rem); background-image: url(assets/media/svg/illustrations/login-visual-5.svg)"></div>  --}}
+					<div class="aside-img d-flex flex-row-fluid bgi-no-repeat bgi-position-x-center" style="background-position-y: calc(100% + 5rem); background-image: url({{ asset('media/svg/illustrations/login-visual-5.svg') }})"></div>
+					<!--end::Aside Bottom-->
+				</div>
+				<!--begin::Aside-->
+				<!--begin::Content-->
+				<div class="login-content flex-row-fluid d-flex flex-column p-10">
+					<!--begin::Wrapper-->
+					<div class="d-flex flex-row-fluid flex-center">
+						<!--begin::Signin-->
+						<div class="login-form">
+							<!--begin::Form-->
+							<form class="form" id="kt_login_singin_form" action="">
+								<!--begin::Title-->
+								<div class="pb-5 pb-lg-15">
+									<h3 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg">Iniciar sesión</h3>
+									<div class="text-muted font-weight-bold font-size-h4">No tienes una cuenta?
+									<a href="custom/pages/login/login-3/signup.html" class="text-primary font-weight-bolder">Regístrate</a></div>
+								</div>
+								<!--begin::Title-->
+								<!--begin::Form group-->
+								<div class="form-group">
+									<label class="font-size-h6 font-weight-bolder text-dark">Correo electrónico</label>
+									<input class="form-control h-auto py-7 px-6 rounded-lg border-0" type="text" name="username" autocomplete="off" />
+								</div>
+								<!--end::Form group-->
+								<!--begin::Form group-->
+								<div class="form-group">
+									<div class="d-flex justify-content-between mt-n5">
+										<label class="font-size-h6 font-weight-bolder text-dark pt-5">Your Name</label>
+										<a href="custom/pages/login/login-3/forgot.html" class="text-primary font-size-h6 font-weight-bolder text-hover-primary pt-5">Forgot Password ?</a>
+									</div>
+									<input class="form-control h-auto py-7 px-6 rounded-lg border-0" type="password" name="password" autocomplete="off" />
+								</div>
+								<!--end::Form group-->
+								<!--begin::Action-->
+								<div class="pb-lg-0 pb-5">
+									<button type="submit" id="kt_login_singin_form_submit_button" class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-3">Sign In</button>
+									<button type="button" class="btn btn-light-primary font-weight-bolder px-8 py-4 my-3 font-size-lg">
+									<span class="svg-icon svg-icon-md">
+										<!--begin::Svg Icon | path:assets/media/svg/social-icons/google.svg-->
+										<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+											<path d="M19.9895 10.1871C19.9895 9.36767 19.9214 8.76973 19.7742 8.14966H10.1992V11.848H15.8195C15.7062 12.7671 15.0943 14.1512 13.7346 15.0813L13.7155 15.2051L16.7429 17.4969L16.9527 17.5174C18.879 15.7789 19.9895 13.221 19.9895 10.1871Z" fill="#4285F4" />
+											<path d="M10.1993 19.9313C12.9527 19.9313 15.2643 19.0454 16.9527 17.5174L13.7346 15.0813C12.8734 15.6682 11.7176 16.0779 10.1993 16.0779C7.50243 16.0779 5.21352 14.3395 4.39759 11.9366L4.27799 11.9466L1.13003 14.3273L1.08887 14.4391C2.76588 17.6945 6.21061 19.9313 10.1993 19.9313Z" fill="#34A853" />
+											<path d="M4.39748 11.9366C4.18219 11.3166 4.05759 10.6521 4.05759 9.96565C4.05759 9.27909 4.18219 8.61473 4.38615 7.99466L4.38045 7.8626L1.19304 5.44366L1.08875 5.49214C0.397576 6.84305 0.000976562 8.36008 0.000976562 9.96565C0.000976562 11.5712 0.397576 13.0882 1.08875 14.4391L4.39748 11.9366Z" fill="#FBBC05" />
+											<path d="M10.1993 3.85336C12.1142 3.85336 13.406 4.66168 14.1425 5.33717L17.0207 2.59107C15.253 0.985496 12.9527 0 10.1993 0C6.2106 0 2.76588 2.23672 1.08887 5.49214L4.38626 7.99466C5.21352 5.59183 7.50242 3.85336 10.1993 3.85336Z" fill="#EB4335" />
+										</svg>
+										<!--end::Svg Icon-->
+									</span>Sign in with Google</button>
+								</div>
+								<!--end::Action-->
+							</form>
+							<!--end::Form-->
+						</div>
+						<!--end::Signin-->
+					</div>
+					<!--end::Wrapper-->
+				</div>
+				<!--end::Content-->
+			</div>
+			<!--end::Login-->
+		</div>
+		<!--end::Main-->
+		<script>var HOST_URL = "{{ route('quick-search') }}";</script>
+
+        {{-- Global Config (global config for global JS scripts) --}}
+        <script>
+            var KTAppSettings = {!! json_encode(config('layout.js'), JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES) !!};
+        </script>
+
+        {{-- Global Theme JS Bundle (used by all pages)  --}}
+        @foreach(config('layout.resources.js') as $script)
+            <script src="{{ asset($script) }}" type="text/javascript"></script>
+        @endforeach
+
+        {{-- Includable JS --}}
+        @yield('scripts')
+	</body>
+	<!--end::Body-->
+</html>
