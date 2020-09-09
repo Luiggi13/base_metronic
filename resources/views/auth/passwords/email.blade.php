2e3cb913d1d52dbd6dc4bc507973f60f9d7e0d3c
Christian Llansola <christian.llansola@gmail.com>
2020-09-09 14:01:54 +0200

    reset password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,79 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
+  <link href="{{ asset('css/pages/login/login-3.css') }}" rel="stylesheet">
+<!--begin::Main-->
+		<div class="d-flex flex-column flex-root">
+			<!--begin::Login-->
+			<div class="login login-3 wizard d-flex flex-column flex-lg-row flex-column-fluid">
+				<!--begin::Aside-->
+				<div class="login-aside d-flex flex-column flex-row-auto">
+					<!--begin::Aside Top-->
+					<div class="d-flex flex-column-auto flex-column pt-lg-40 pt-15">
+						<!--begin::Aside header-->
+						<a href="#" class="login-logo text-center pt-lg-25 pb-10">
+							<img src="{{ asset('/nixus/media/logos/nixus-1.png') }}" class="max-h-60px" alt="" />
+						</a>
+						<!--end::Aside header-->
+						<!--begin::Aside Title-->
+						<h3 class="font-weight-bolder text-center font-size-h4 text-dark-50 line-height-xl">User Experience &amp; Interface Design
+						<br />Strategy SaaS Solutions</h3>
+						<!--end::Aside Title-->
+					</div>
+					<!--end::Aside Top-->
+					<!--begin::Aside Bottom-->
+					{{--  <div class="aside-img d-flex flex-row-fluid bgi-no-repeat bgi-position-x-center" style="background-position-y: calc(100% + 5rem); background-image: url(assets/media/svg/illustrations/login-visual-5.svg)"></div>  --}}
+					<div class="aside-img d-flex flex-row-fluid bgi-no-repeat bgi-position-x-center" style="background-position-y: calc(100% + 5rem); background-image: url({{ asset('media/svg/illustrations/login-visual-5.svg') }})"></div>
+					<!--end::Aside Bottom-->
+				</div>
+				<!--begin::Aside-->
+				<!--begin::Content-->
+				<div class="login-content flex-row-fluid d-flex flex-column p-10">
+					<!--begin::Wrapper-->
+					<div class="d-flex flex-row-fluid flex-center">
+						<!--begin::Signin-->
+						<div class="login-form">
+                        @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+							<!--begin::Form-->
+							<form class="form" id="kt_login_singin_form" method="POST" action="{{ route('password.email') }}">
+                            @csrf
+								<!--begin::Title-->
+								<div class="pb-5 pb-lg-15">
+									<h3 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg">{{ __('Reset Password') }}</h3>
+								</div>
+								<!--begin::Title-->
+								<!--begin::Form group-->
+								<div class="form-group">
+									<label for="email" class="font-size-h6 font-weight-bolder text-dark">{{ __('E-Mail Address') }}</label>
+									<input class="form-control h-auto py-7 px-6 rounded-lg border-0 @error('email') is-invalid @enderror" id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus />
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
+								</div>
+								<!--end::Form group-->
+								<!--begin::Action-->
+								<div class="pb-lg-0 pb-5">
+									<button type="submit" id="kt_login_singin_form_submit_button" class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-3">{{ __('Send Password Reset Link') }}</button>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+								</div>
+								<!--end::Action-->
+							</form>
+							<!--end::Form-->
+						</div>
+						<!--end::Signin-->
+					</div>
+					<!--end::Wrapper-->
+				</div>
+				<!--end::Content-->
+			</div>
+			<!--end::Login-->
+		</div>
+		<!--end::Main-->
 @endsection
