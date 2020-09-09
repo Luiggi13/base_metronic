7a859f1f21c61fc65acc99cfa420f3b9d27f7d38
Christian Llansola <christian.llansola@gmail.com>
2020-09-09 11:28:17 +0200

    boton logout to test

--- resources/views/layout/base/_aside.blade.php
+++ resources/views/layout/base/_aside.blade.php
@@ -4,51 +4,68 @@
     $kt_logo_image = 'logo-light.png';
 @endphp
 
-@if (config('layout.brand.self.theme') === 'light')
-    @php $kt_logo_image = 'logo-dark.png' @endphp
-@elseif (config('layout.brand.self.theme') === 'dark')
-    @php $kt_logo_image = 'logo-light.png' @endphp
-@endif
-
-<div class="aside aside-left {{ Metronic::printClasses('aside', false) }} d-flex flex-column flex-row-auto" id="kt_aside">
-
-    {{-- Brand --}}
-    <div class="brand flex-column-auto {{ Metronic::printClasses('brand', false) }}" id="kt_brand">
-        <div class="brand-logo">
-            <a href="{{ url('/') }}">
-                <img alt="{{ config('app.name') }}" src="{{ asset('media/logos/'.$kt_logo_image) }}"/>
-            </a>
-        </div>
-
-        @if (config('layout.aside.self.minimize.toggle'))
-            <button class="brand-toggle btn btn-sm px-0" id="kt_aside_toggle">
-                {{ Metronic::getSVG("media/svg/icons/Navigation/Angle-double-left.svg", "svg-icon-xl") }}
-            </button>
+@if(config('layout.brand.self.theme') === 'light')
+    @php$kt_logo_image = 'logo-dark.png' @endphp
+        <blade
+            elseif|%20(config(%26%2339%3Blayout.brand.self.theme%26%2339%3B)%20%3D%3D%3D%20%26%2339%3Bdark%26%2339%3B) />
+        @php$kt_logo_image = 'logo-light.png' @endphp
         @endif
 
-    </div>
+        <div class="aside aside-left {{ Metronic::printClasses('aside', false) }} d-flex flex-column flex-row-auto"
+            id="kt_aside">
 
-    {{-- Aside menu --}}
-    <div class="aside-menu-wrapper flex-column-fluid" id="kt_aside_menu_wrapper">
+            {{-- Brand --}}
+            <div class="brand flex-column-auto {{ Metronic::printClasses('brand', false) }}"
+                id="kt_brand">
+                <div class="brand-logo">
+                    <a href="{{ url('/') }}">
+                        <img alt="{{ config('app.name') }}"
+                            src="{{ asset('media/logos/'.$kt_logo_image) }}" />
+                    </a>
+                </div>
 
-        @if (config('layout.aside.self.display') === false)
-            <div class="header-logo">
-                <a href="{{ url('/') }}">
-                    <img alt="{{ config('app.name') }}" src="{{ asset('media/logos/'.$kt_logo_image) }}"/>
-                </a>
+                @if(config('layout.aside.self.minimize.toggle'))
+                    <button class="brand-toggle btn btn-sm px-0" id="kt_aside_toggle">
+                        {{ Metronic::getSVG("media/svg/icons/Navigation/Angle-double-left.svg", "svg-icon-xl") }}
+                    </button>
+                @endif
+
             </div>
-        @endif
 
-        <div
-            id="kt_aside_menu"
-            class="aside-menu my-4 {{ Metronic::printClasses('aside_menu', false) }}"
-            data-menu-vertical="1"
-            {{ Metronic::printAttrs('aside_menu') }}>
+            {{-- Aside menu --}}
+            <div class="aside-menu-wrapper flex-column-fluid" id="kt_aside_menu_wrapper">
+                <div class="header-logo mt-3">
+                <a href="{{ url('/') }}">
+                    <img alt="{{ config('app.name') }}"
+                        src="{{ asset('media/logos/'.$kt_logo_image) }}" />
+                </a>
+                </div>
+                    <a class="dropdown-item mt-3 mb-3" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();" style="padding: 9px 25px;">
+                        <i class="fas fa-sign-out-alt"></i>
+                        <small class="text-muted font-size-sm ml-2">{{ __('Logout') }}</small>
+                    </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
+            @if(config('layout.aside.self.display') === false)
+                <div class="header-logo">
+                    <a href="{{ url('/') }}">
+                        <img alt="{{ config('app.name') }}"
+                            src="{{ asset('media/logos/'.$kt_logo_image) }}" />
+                    </a>
+                </div>
+            @endif
 
-            <ul class="menu-nav {{ Metronic::printClasses('aside_menu_nav', false) }}">
-                {{ Menu::renderVerMenu(config('menu_aside.items')) }}
-            </ul>
+            <div id="kt_aside_menu"
+                class="aside-menu my-4 {{ Metronic::printClasses('aside_menu', false) }}"
+                data-menu-vertical="1" {{ Metronic::printAttrs('aside_menu') }}>
+
+                <ul class="menu-nav {{ Metronic::printClasses('aside_menu_nav', false) }}">
+                    {{ Menu::renderVerMenu(config('menu_aside.items')) }}
+                </ul>
+            </div>
         </div>
-    </div>
 
-</div>
+        </div>
