7618d7e0156bf4ed64704731b63af3700f4c18e2
Christian Llansola <christian.llansola@gmail.com>
2020-09-16 15:35:46 +0200

    dashboard css

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 
 {{-- Dashboard 1 --}}
+  <link href="{{ asset('css/pages/dashboard/dashboard.css') }}" rel="stylesheet">
 
 <div class="row">
     <div class="col-lg-12 page-title-section">
@@ -147,40 +148,6 @@
 <div class="col-md-12 mt-5">
 <div class="datatable datatable-bordered datatable-head-custom" id="kt_datatable"></div></div>
 </div>
-
-{{--  <div class="row">
-        <div class="col-lg-6 col-xxl-4">
-@include('pages.widgets._widget-1', ['class' => 'card-stretch gutter-b'])
-        </div>
-
-        <div class="col-lg-6 col-xxl-4">
-@include('pages.widgets._widget-2', ['class' => 'card-stretch gutter-b'])
-        </div>
-
-        <div class="col-lg-6 col-xxl-4">
-@include('pages.widgets._widget-3', ['class' => 'card-stretch card-stretch-half gutter-b'])
-@include('pages.widgets._widget-4', ['class' => 'card-stretch card-stretch-half gutter-b'])
-        </div>
-
-        <div class="col-lg-6 col-xxl-4 order-1 order-xxl-1">
-@include('pages.widgets._widget-5', ['class' => 'card-stretch gutter-b'])
-        </div>
-
-        <div class="col-xxl-8 order-2 order-xxl-1">
-@include('pages.widgets._widget-6', ['class' => 'card-stretch gutter-b'])
-        </div>
-
-        <div class="col-lg-6 col-xxl-4 order-1 order-xxl-2">
-@include('pages.widgets._widget-7', ['class' => 'card-stretch gutter-b'])
-        </div>
-
-        <div class="col-lg-6 col-xxl-4 order-1 order-xxl-2">
-@include('pages.widgets._widget-8', ['class' => 'card-stretch gutter-b'])
-        </div>
-
-        <div class="col-lg-12 col-xxl-4 order-1 order-xxl-2">
-@include('pages.widgets._widget-9', ['class' => 'card-stretch gutter-b'])
-        </div>--}}
 <style>
     .page-title-section {
         width: 100%;
