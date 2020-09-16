df89a8e42cf238209f6b7a91406f1057bf563f28
Christian Llansola <christian.llansola@gmail.com>
2020-09-16 15:33:48 +0200

    dashboard nixus template

--- resources/metronic/js/pages/crud/ktdatatable/advanced/vertical.js
+++ resources/metronic/js/pages/crud/ktdatatable/advanced/vertical.js
@@ -47,90 +47,22 @@ var KTDefaultDatatableDemo = function () {
 					selector: false,
 					textAlign: 'center',
 				}, {
-					field: 'OrderID',
-					title: 'Order ID',
+					field: 'zona',
+					title: 'Zona',
 				}, {
-					field: 'Country',
-					title: 'Country',
+					field: 'entry',
+					title: 'Entrada',
 					template: function(row) {
 						return row.Country + ' ' + row.ShipCountry;
 					},
 				}, {
-					field: 'CompanyEmail',
+					field: 'valueMachine',
 					width: 150,
-					title: 'Email',
+					title: 'Valor',
 				}, {
-					field: 'ShipAddress',
-					title: 'Ship Address',
-				}, {
-					field: 'ShipDate',
-					title: 'Ship Date',
-					type: 'date',
-					format: 'MM/DD/YYYY',
-				}, {
-					field: 'CompanyName',
-					title: 'Company Name',
-				}, {
-					field: 'Status',
-					title: 'Status',
-					// callback function support for column rendering
-					template: function(row) {
-						var status = {
-							1: {'title': 'Pending', 'class': 'label-light-primary'},
-							2: {'title': 'Delivered', 'class': ' label-light-danger'},
-							3: {'title': 'Canceled', 'class': ' label-light-primary'},
-							4: {'title': 'Success', 'class': ' label-light-success'},
-							5: {'title': 'Info', 'class': ' label-light-info'},
-							6: {'title': 'Danger', 'class': ' label-light-danger'},
-							7: {'title': 'Warning', 'class': ' label-light-warning'},
-						};
-
-						return '<span class="label label-lg font-weight-bold' + status[row.Status].class + ' label-inline">' + status[row.Status].title + '</span>';
-					},
-				}, {
-					field: 'Type',
-					title: 'Type',
-					autoHide: false,
-					// callback function support for column rendering
-					template: function(row) {
-						var status = {
-							1: {'title': 'Online', 'state': 'danger'},
-							2: {'title': 'Retail', 'state': 'primary'},
-							3: {'title': 'Direct', 'state': 'success'},
-						};
-						return '<span class="label label-' + status[row.Type].state + ' label-dot mr-2"></span><span class="font-weight-bold text-' + status[row.Type].state + '">' +
-								status[row.Type].title + '</span>';
-					},
-				}, {
-					field: 'Actions',
-					title: 'Actions',
-					sortable: false,
-					width: 110,
-					overflow: 'visible',
-					autoHide: false,
-					template: function() {
-						return '\
-							<div class="dropdown dropdown-inline">\
-								<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" data-toggle="dropdown">\
-	                                <i class="la la-cog"></i>\
-	                            </a>\
-							  	<div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">\
-									<ul class="nav nav-hoverable flex-column">\
-							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-edit"></i><span class="nav-text">Edit Details</span></a></li>\
-							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-leaf"></i><span class="nav-text">Update Status</span></a></li>\
-							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-print"></i><span class="nav-text">Print</span></a></li>\
-									</ul>\
-							  	</div>\
-							</div>\
-							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Edit details">\
-								<i class="la la-edit"></i>\
-							</a>\
-							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Delete">\
-								<i class="la la-trash"></i>\
-							</a>\
-						';
-					},
-				}],
+					field: 'machineHour',
+					title: 'Hora de la máquina',
+				}]
 
 		});
 
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,21 +1,354 @@
+{{-- Extends layout --}}
+@extends('layout.default')
 
-The default interactive shell is now zsh.
-To update your account to use zsh, please run `chsh -s /bin/zsh`.
-For more details, please visit https://support.apple.com/kb/HT208050.
-<<La normalidad es las contraseñas y usuarios por raw body>>
-/Users/bebito/Downloads/metronic_v7.1.0/theme/html_laravel/demo1/skeleton:   [git:tailwindo]
-🍼🔝  BEBITOS 🔝🍼=>
+{{-- Content --}}
+@section('content')
+
+{{-- Dashboard 1 --}}
+
+<div class="row">
+    <div class="col-lg-12 page-title-section">
+        <div class="app-page-title">
+
+            <div class="page-title-wrapper">
+                <div class="row">
+                    <div class="col-md-6">
+
+                        <div class="page-title-heading">
+                            <div class="page-title-icon"><i class="fa fa-comment-alt text-success"></i></div>
+                            <div>
+                                Nombre de la máquina a visualizar
+                                <div class="page-title-subheading">Quis excepteur voluptate elit quis nisi eiusmod
+                                    aliqua deserunt.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="page-title-actions">
+                            <select class="form-control selectpicker" title="Opciones" data-style="btn-primary">
+                                <option>Opción 1</option>
+                                <option>Opción 2</option>
+                                <option>Opción 3</option>
+                                <option>Opción 4</option>
+                                <option>Opción 5</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
 
 
+</div>
+<div class="row">
+    <div class="col-md-4 col-lg-4">
 
 
+        <div class="card card-custom bg-danger">
+            <div class="card-header border-0">
+                <div class="card-title">
+                    <h3 class="card-label text-white">
+                        Producción últimas 24 horas
+                    </h3>
+                </div>
+                <div class="card-toolbar">
+                    <p class="unidades-fabricadas">2155 uds.</p>
+                </div>
+            </div>
 
 
+            <i class="fa fa-wrench text-white"></i>
+        </div>
 
 
+    </div>
+
+    <div class="col-md-4 col-lg-4">
 
 
+        <div class="card card-custom bg-success">
+            <div class="card-header border-0">
+                <div class="card-title">
+                    <h3 class="card-label text-white">
+                        Alarmas de máquina
+                    </h3>
+                </div>
+                <div class="card-toolbar">
+                    <p class="alarmas-maquina text-black">3 alarmas</p>
+                </div>
+            </div>
+                <hr>
+                <div class="card-body">
+                    <ul class="mensajes">
+                    <li>
+                        <span>Parada no justificada</span> en:  máquina nombre
+                        <i class="fa fa-eye text-white"></i>
+                    </li>
+                    <li>
+                        <span>Parada no justificada</span> en:  máquina nombre de la máquina
+                        <i class="fa fa-eye text-white"></i>
+                    </li>
+                    <li>
+                        <span>Parada no justificada</span> en:  máquina nombre de la máquina
+                        <i class="fa fa-eye text-white"></i>
+                    </li>
+                    </ul>
+                </div>
+
+            <i class="fa fa-exclamation-triangle text-white"></i>
+        </div>
 
 
+    </div>
+
+ <div class="col-md-4 col-lg-4">
 
 
+        <div class="card card-custom bg-success">
+            <div class="card-header border-0">
+                <div class="card-title">
+                    <h3 class="card-label text-white">
+                        Mensajes de información
+                    </h3>
+                </div>
+                <div class="card-toolbar">
+                    <p class="alarmas-maquina text-black">3 mensajes</p>
+                </div>
+            </div>
+                <hr>
+                <div class="card-body">
+                    <ul class="mensajes">
+                    <li>
+                        <span>Información</span> en:  máquina nombre
+                        <i class="fa fa-eye text-white"></i>
+                    </li>
+                    <li>
+                        <span>Información</span> en:  máquina nombre de la máquina
+                        <i class="fa fa-eye text-white"></i>
+                    </li>
+                    <li>
+                        <span>Información</span> en:  máquina nombre de la máquina
+                        <i class="fa fa-eye text-white"></i>
+                    </li>
+                    </ul>
+                </div>
+
+            <i class="fa fa-exclamation-triangle text-white"></i>
+        </div>
+
+
+    </div>
+
+</div>
+<div class="row">
+<div class="col-md-12 mt-5">
+<div class="datatable datatable-bordered datatable-head-custom" id="kt_datatable"></div></div>
+</div>
+
+{{--  <div class="row">
+        <div class="col-lg-6 col-xxl-4">
+@include('pages.widgets._widget-1', ['class' => 'card-stretch gutter-b'])
+        </div>
+
+        <div class="col-lg-6 col-xxl-4">
+@include('pages.widgets._widget-2', ['class' => 'card-stretch gutter-b'])
+        </div>
+
+        <div class="col-lg-6 col-xxl-4">
+@include('pages.widgets._widget-3', ['class' => 'card-stretch card-stretch-half gutter-b'])
+@include('pages.widgets._widget-4', ['class' => 'card-stretch card-stretch-half gutter-b'])
+        </div>
+
+        <div class="col-lg-6 col-xxl-4 order-1 order-xxl-1">
+@include('pages.widgets._widget-5', ['class' => 'card-stretch gutter-b'])
+        </div>
+
+        <div class="col-xxl-8 order-2 order-xxl-1">
+@include('pages.widgets._widget-6', ['class' => 'card-stretch gutter-b'])
+        </div>
+
+        <div class="col-lg-6 col-xxl-4 order-1 order-xxl-2">
+@include('pages.widgets._widget-7', ['class' => 'card-stretch gutter-b'])
+        </div>
+
+        <div class="col-lg-6 col-xxl-4 order-1 order-xxl-2">
+@include('pages.widgets._widget-8', ['class' => 'card-stretch gutter-b'])
+        </div>
+
+        <div class="col-lg-12 col-xxl-4 order-1 order-xxl-2">
+@include('pages.widgets._widget-9', ['class' => 'card-stretch gutter-b'])
+        </div>--}}
+<style>
+    .page-title-section {
+        width: 100%;
+        padding: 1.5rem;
+        text-align: left;
+        border-bottom: #e9ecef solid 1px;
+        background-image: linear-gradient(to right, #434343 0%, black 100%) !important;
+        border-radius: .3rem;
+        color: #fff;
+        margin-bottom: 1.5rem !important;
+    }
+
+    .app-page-title {
+        font-size: 1.25rem;
+        font-weight: 400;
+        display: flex;
+        align-content: center;
+        align-items: center;
+        margin: 0;
+        padding: 0;
+    }
+
+    .app-page-title .page-title-icon {
+        font-size: 2rem;
+        display: flex;
+        align-items: center;
+        align-content: center;
+        text-align: center;
+        padding: .83333rem;
+        margin: 0 30px 0 0;
+        background: #fff;
+        box-shadow: 0 0.46875rem 2.1875rem rgba(4, 9, 20, 0.03), 0 0.9375rem 1.40625rem rgba(4, 9, 20, 0.03), 0 0.25rem 0.53125rem rgba(4, 9, 20, 0.05), 0 0.125rem 0.1875rem rgba(4, 9, 20, 0.03);
+        border-radius: 50px;
+        width: 60px;
+        height: 60px;
+    }
+
+    .fa-comment-alt,
+    .fas.fa-server {
+        display: inline-block;
+        margin: 0 auto;
+        speak: none;
+        font-size: 25px;
+        font-style: normal;
+        font-weight: normal;
+        font-variant: normal;
+        text-transform: none;
+        line-height: 1;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    .app-page-title .page-title-subheading {
+        padding: 3px 0 0;
+        font-size: .88rem;
+        opacity: .6;
+    }
+
+    .app-page-title .page-title-heading {
+        font-size: 1.25rem;
+        font-weight: 400;
+        display: flex;
+        align-content: center;
+        align-items: center;
+    }
+
+    .app-page-title .page-title-actions {
+        margin-left: auto;
+        float: right;
+        width: 20%;
+    }
+
+    .filter-option-inner {
+        text-align: center;
+    }
+
+    .page-title-wrapper {
+        width: 100%;
+    }
+
+    .card-title {
+        text-align: right;
+        width: 100%;
+        margin-top: 20px !important;
+    }
+
+    div.card.card-custom>.card-header .card-title .card-label {
+        margin: 0;
+        width: 100%;
+    }
+
+    .card-header {
+        padding: 2rem 1.25rem;
+    }
+
+    .card-toolbar {
+        width: 100%;
+    }
+
+    p.unidades-fabricadas {
+        width: 100%;
+        text-align: right;
+        color: #fff;
+        font-size: 30px;
+    }
+    p.alarmas-maquina {
+        width: 100%;
+        text-align: right;
+        color: #fff;
+        font-size: 20px;
+        padding-top:15px;
+    }
+
+    .card i {
+        font-size: 30px;
+        position: absolute;
+        top: 25px;
+
+        left: 15px;
+        color: rgba(255, 255, 255, .5) !important;
+    }
+    .card i.fa-exclamation-triangle {
+    }
+.text-green {
+    color:#1BC5BD !important
+}
+.text-black {
+    color:#000 !important
+}
+
+hr {
+    height: 0px;
+background-color: rgba(255,255,255,.7);
+width: 100%;
+}
+ul.mensajes{
+    list-style-type: none;
+padding: 0;
+margin: 0;
+}
+ul.mensajes li{
+    position:relative;
+padding: 10px 0;
+}
+ul.mensajes li span{
+    font-weight:bold
+}
+ul.mensajes li i {
+left: initial !important;
+right: 0;
+top: 12px;
+position:absolute;font-size:15px;
+
+        color: rgba(255, 255, 255, 1) !important;
+    }
+
+    #kt_wrapper {
+        padding-top:30!important;
+    }
+</style>
+
+@endsection
+
+{{-- Scripts Section --}}
+@section('scripts')
+{{-- <script src="assets/js/pages/features/charts/apexcharts.js?asdsad"></script> --}}
+
+<script src="{{ asset('js/pages/widgets.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/pages/crud/ktdatatable/advanced/vertical.js') }}" type="text/javascript"></script>
+
+@endsection
